c16df0d5c0613f0e8564b45e0a231d418303f0b1
Lavkush Kumar <lavkushry@gmail.com>
2024-01-05 17:01:18 +0530

    Removed Unwated Files

--- src/num_w_even_Num_of_digit_leetcode_ls.java
+++
@@ -1,8 +0,0 @@
-public class num_w_even_Num_of_digit_leetcode_ls {
-    public static void main(String[] args) {
-        // find the Number with Even Number of Digits
-        int[] arr={12,345,1,6,7896};
-
-    }
-
-}
