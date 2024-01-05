6a735fa3cbea56470144b972662a662cb20d0f03
Lavkush Kumar <lavkushry@gmail.com>
2024-01-05 12:21:55 +0530

    Pattern is 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5

---
+++ src/Pattern_Programming/Pattern_4.java
@@ -0,0 +1,20 @@
+package Pattern_Programming;
+/*
+Pattern is
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+*/
+public class Pattern_4 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
