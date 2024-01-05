d5a03f857348c05ca007e0fa0143c17342a7ad50
Lavkush Kumar <lavkushry@gmail.com>
2024-01-05 17:12:33 +0530

    Added more Efficient Prime Factor

--- src/Mathematics/Prime_Fact01.java
+++ src/Mathematics/Prime_Fact01.java
@@ -2,17 +2,19 @@ package Mathematics;
 
 import java.util.Scanner;
 // Find the Prime Factors of N
-public class Prime_factors {
+public class Prime_Fact01 {
     public static void primeFactors(int n){
         int i=2;
-        while (n>1){
+        while (i*i<=n){
             while (n%i==0){
-                System.out.print(i+",");
+                System.out.print(i+"x");
                 n=n/i;
             }
             i++;
         }
-
+        if (n>1) {
+            System.out.println(n);
+        }
     }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
