536ba17df38cb29e66bdc169bf474e0527575497
Lavkush Kumar <lavkushry@gmail.com>
2024-01-05 12:13:25 +0530

    added Comment

--- src/Pattern_Programming/Patter1.java
+++ src/Pattern_Programming/Patter1.java
@@ -1,12 +1,23 @@
 package Pattern_Programming;
-
+/* Pattern Like
+01 02 03 04 05
+06 07 08 09 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
+*/
 public class Patter1 {
     public static void main(String[] args) {
-        int n=25;
-        int a=1;
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=5; j++){
-                System.out.print(a++);
+        int count=1;
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(count<10){
+                    System.out.print(0);
+                }
+                    System.out.print(count + " ");
+
+                count++;
             }
             System.out.println();
         }
