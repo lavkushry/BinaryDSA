1812097a33ee024ee13a3a067ee77efb80e2fcdd
Lavkush Kumar <lavkushry@gmail.com>
2024-01-06 13:36:09 +0530

    Finding the All Set Bit using Brian Karnighan's Algorithm  method

--- src/Bit_Manuplation/Briam_KarnighanRSMB.java
+++ src/Bit_Manuplation/Briam_KarnighanRSMB.java
@@ -1,11 +1,17 @@
 package Bit_Manuplation;
-// Finding the Right Most Set Bit using Enhanced/Efficient method
+// Finding the All Set Bit using Brian Karnighan's Algorithm  method
 
 import java.util.Scanner;
 
-public class Find_Righ_most_Set_bit_efficient {
-    public static int findRSMB(int n){
-        return ((int)(Math.log10(n^(n&(n-1)))/Math.log10(2))+1);
+
+public class Briam_KarnighanRSMB {
+    public static int findRSMB( int n){
+        int count=0;
+        while (n>0){
+            n=n*(n-1);
+            count++;
+        }
+        return count;
     }
     public static void main(String[] args) {
         System.out.println(" Enter the decimal No ");
@@ -13,4 +19,6 @@ public class Find_Righ_most_Set_bit_efficient {
         int n= sc.nextInt();
         System.out.println(findRSMB(n));
     }
+
+
 }
