1e7c33ecad601102bde76a8c51165a3cd9eb8705
lavkushry <lavkushry@gmail.com>
2024-01-12 23:43:47 +0530

    * * * * * * * * * * * * * * * * * * * * * * * * *

---
+++ src/Pattern_Programming/Pattern_16.java
@@ -0,0 +1,22 @@
+package Pattern_Programming;
+/*
+Pattern Programming of
+
+*
+* * *
+* * * * *
+* * * * * * *
+* * * * * * * * *
+
+*/
+public class Pattern_16 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=(2*i)-1 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
