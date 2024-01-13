38a2a95f9c03870b6e6c6965aac3e83d335b6b9e
lavkushry <lavkushry@gmail.com>
2024-01-13 13:45:45 +0530

    modified program

--- src/Pattern_Programming/Pattern_21.java
+++ src/Pattern_Programming/Pattern_21.java
@@ -1,20 +1,27 @@
 package Pattern_Programming;
 /*
 Print this program
+modified
 1 1 1 1 1 2
-2 2 2 2 2 3
+3 2 2 2 2 2
 3 3 3 3 3 4
-4 4 4 4 4 5
+5 4 4 4 4 4
 5 5 5 5 5 6
+
 */
 public class Pattern_21 {
     public static void main(String[] args) {
         int n=5;
         for (int i = 1; i <=n ; i++) {
+            if(i%2==0){
+                System.out.print(i+1+" ");
+            }
             for (int j = 1; j <=n ; j++) {
                 System.out.print(i+" ");
             }
-            System.out.print(i+1);
+            if (i%2!=0){
+                System.out.print(i+1);
+            }
             System.out.println();
         }
     }
