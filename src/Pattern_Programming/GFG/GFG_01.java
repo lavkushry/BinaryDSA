e55d96fef1faa041651b8363e1c38a4d5bbce555
lavkushry <lavkushry@gmail.com>
2024-01-13 18:34:22 +0530

    Modified

--- src/Pattern_Programming/GFG/GFG_01.java
+++ src/Pattern_Programming/GFG/GFG_01.java
@@ -1,25 +1,66 @@
 package Pattern_Programming.GFG;
+/*
+Pattern Programming of
+1
+4
+1 1 1 1 1 1 1
+1 2 2 2 2 2 1
+1 2 3 3 3 2 1
+1 2 3 4 3 2 1
+1 2 3 3 3 2 1
+1 2 2 2 2 2 1
+1 1 1 1 1 1 1
+*/
 
-public class GFG_01 {
-    public static void main (String[] args) {
-        int n = 7;
-        int value=1;
+import java.util.Scanner;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+public class GFG_01 {static void boxPattern(int n) {
+    int ind = n * 2 - 1;
+    int[][] mat = new int[ind][ind];
+    int count = 1;
 
-                int minValue = Math.min(i, j);
+    int top = 0, left = 0;
+    int right = ind - 1, down = ind - 1;
 
-//                int value = n - minValue + 1;
-                while (value<=n/2){
-                    value = n - minValue + 1;
-                    n--;
-                }
-                System.out.print(value);
+    while (count <= n) {
+        // top row
+        for (int i = left; i <= right; i++)
+            mat[top][i] = count;
+        // left column
+        for (int j = top + 1; j <= down; j++)
+            mat[j][left] = count;
+        // right column
+        for (int k = left + 1; k <= right; k++)
+            mat[k][right] = count;
+        // bottom row
+        for (int l = top + 1; l <= down; l++)
+            mat[down][l] = count;
 
-            }
-            System.out.println();
+        count++;
+        top++;
+        right--;
+        left++;
+        down--;
+    }
+
+    for (int i = 0; i < ind; i++) {
+        for (int j = 0; j < ind; j++) {
+            System.out.print(mat[i][j] + " ");
+        }
+        System.out.println();
+    }
+}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+            int N = scanner.nextInt();
+            boxPattern(N);
         }
 
+        scanner.close();
     }
+
 }
