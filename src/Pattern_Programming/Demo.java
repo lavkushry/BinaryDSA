c9135a3d73bca5a07ebf5c502e4a7c3c6859789a
lavkushry <lavkushry@gmail.com>
2024-01-13 12:50:52 +0530

    Added new Pattern

--- src/Pattern_Programming/Demo.java
+++ src/Pattern_Programming/Demo.java
@@ -1,18 +1,21 @@
 package Pattern_Programming;
 
+import java.util.Scanner;
 public class Demo {
+
     public static void main(String[] args) {
-        int n=5;
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j >= (n-1)-i) {
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+
+        // Consume the remaining newline character after nextDouble()
+//        scan.nextLine();  // Add this line to clear the newline character
+
+
+
+        System.out.println("String: " +s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
---
+++ src/Pattern_Programming/Pattern_21.java
@@ -0,0 +1,21 @@
+package Pattern_Programming;
+/*
+Print this program
+1 1 1 1 1 2
+2 2 2 2 2 3
+3 3 3 3 3 4
+4 4 4 4 4 5
+5 5 5 5 5 6
+*/
+public class Pattern_21 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.print(i+1);
+            System.out.println();
+        }
+    }
+}
