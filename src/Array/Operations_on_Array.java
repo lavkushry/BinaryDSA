f7bdaa01b5fcb4636626503f8fe0469a1df7d63c
lavkushry <lavkushry@gmail.com>
2024-01-16 19:37:30 +0530

    Program of Stock buy and Sell Geeks for Geeks

--- src/Array/Operations_on_Array.java
+++ src/Array/Operations_on_Array.java
@@ -11,10 +11,48 @@ public class Operations_on_Array {
         }
         return -1;
     }
-    public static void main(String[] args) {
-        Operations_on_Array n=new Operations_on_Array();
-        int[] arr={20,5,7,25};
 
-        System.out.println(n.search(arr,4,7));
+    int Insert(int[] arr, int n, int x, int cap, int pos){
+        if(n==cap)
+        {
+            return  n;
+        }
+        int index=pos-1;
+        for(int i=1; i>index; i--){
+            arr[i+1]=arr[i];
+        }
+        arr[index]=x;
+        return (n+1);
+    }
+
+    int Delete(int[] arr,int n,int x){
+        int i;
+        for ( i = 0; i <n ; i++) {
+            if (arr[i]==x)
+            {
+                break;
+            }
+            if(i == n){
+                return n;
+            }
+            for (int j = 1; j <n-1 ; j++) {
+                arr[j]=arr[j+1];
+            }
+
+        }
+        return (n-1);
+    }
+    public static void main(String[] args) {
+        Operations_on_Array obj=new Operations_on_Array();
+        int[] arr={20,5,7,25};
+        int n=4;
+        int x=7;
+        int cap=5;
+        int pos=2;
+
+        System.out.println(obj.search(arr,n,x));
+        System.out.println(obj.Insert(arr,n,x,cap,pos));
+        System.out.println(obj.Delete(arr,n,x));
+
     }
 }
---
+++ src/Array/Stock_buy_and_Sell.java
@@ -0,0 +1,44 @@
+package Array;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Stock_buy_and_Sell {
+
+    static int Max_profit(int price[],int start,int end)
+    {
+        if(end <= start){
+            return 0;
+        }
+        int profit=0;
+        for (int i = start; i <end ; i++) {
+            for (int j = i+1; j <end; j++) {
+                if(price[j]>price[i])
+                {
+                    int curr_profit=price[i]-price[j]+ + Max_profit(price, start, i - 1) + Max_profit(price, j + 1, end);
+                    profit = Math.max(profit, curr_profit);
+                }
+
+            }
+        }
+        return profit;
+    }
+    static int Max_profit_Efficient(int price[],int n){
+        int profit=0;
+        for (int i = 1; i <n ; i++) {
+            if(price[i] > price[i-1])
+            {
+                profit+=(price[i]-price[i-1]);
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] price={1,5,3,5,8,12};
+        int start=0;
+        int end=5;
+
+        System.out.println(Max_profit(price, 0, end));
+        System.out.println(Max_profit_Efficient(price, 5));
+    }
+}
