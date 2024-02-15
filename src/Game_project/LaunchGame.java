fd9cfc9efef2fcc3320fb1ce15086c5651881a3d
lavkushry <lavkushry@gmail.com>
2024-02-15 21:58:59 +0530

    Added New Program

---
+++ src/Game_project/LaunchGame.java
@@ -0,0 +1,77 @@
+package Game_project;
+
+import java.util.Scanner;
+
+class Guesser
+{
+    int guessNum;
+   int gussingNumber(){
+        System.out.println("Guesser Kindly guess the Number");
+        Scanner scan = new Scanner(System.in);
+        guessNum=scan.nextInt();
+        return guessNum;
+    }
+}
+
+class Player
+{
+    int guessNum;
+    int gussingNumber()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player! Kindly guess the Numeber");
+        guessNum=scan.nextInt();
+        return guessNum;
+    }
+}
+class Umpire
+{
+    int numFromGuesser;
+    int numFromPlayer1;
+    int numFromPlayer2;
+    int numFromPlayer3;
+
+    void collectNumFromGuesser()
+    {
+        Guesser g=new Guesser();
+       numFromGuesser= g.gussingNumber();
+
+    }
+    void collectNumFromPlayers()
+    {
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+        numFromPlayer1 =p1.gussingNumber();
+        numFromPlayer2 =p2.gussingNumber();
+        numFromPlayer3 =p3.gussingNumber();
+
+    }
+    void compare()
+    {
+        if (numFromGuesser==numFromPlayer1)
+        {
+            System.out.println("Player1 Won the game");
+        }
+        else if (numFromGuesser==numFromPlayer2)
+        {
+            System.out.println("Player2 Won the game");
+        } else if (numFromGuesser==numFromPlayer3)
+        {
+            System.out.println("Player3 Won the game");
+        }else {
+            System.out.println("Aap game haar gye");
+        }
+
+    }
+}
+public class LaunchGame {
+    public static void main(String[] args) {
+        System.out.println("Game Started");
+        Umpire u = new Umpire();
+        u.collectNumFromGuesser();
+        u.collectNumFromPlayers();
+        u.compare();
+
+    }
+}
